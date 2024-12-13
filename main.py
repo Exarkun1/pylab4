f4c7af2104093aa635bad54a69289084717fa3a9
Rafael Abeldinov <raf2003@bk.ru>
2024-12-13 11:42:24 +0400

    add docs

--- main.py
+++ main.py
@@ -6,20 +6,23 @@ import utils
 from data_storage import DataStorage
 from plots import Drawer
 
-def is_exit(comand: list[str]) -> bool:
-    return len(comand) == 1 and comand[0] == "exit"
+def is_exit(command: list[str]) -> bool:
+    return len(command) >= 1 and command[0] == "exit"
 
-def is_load(comand: list[str]) -> bool:
-    return len(comand) == 2 and comand[0] == "load"
+def is_load(command: list[str]) -> bool:
+    return len(command) == 2 and command[0] == "load"
 
-def is_save(comand: list[str]) -> bool:
-    return len(comand) == 2 and comand[0] == "save"
+def is_save(command: list[str]) -> bool:
+    return len(command) == 2 and command[0] == "save"
 
-def is_download(comand: list[str]) -> bool:
-    return len(comand) == 5 and comand[0] == "download"
+def is_download(command: list[str]) -> bool:
+    return len(command) == 5 and command[0] == "download"
 
-def is_draw(comand: list[str]) -> bool:
-    return len(comand) == 1 and comand[0] == "draw"
+def is_draw(command: list[str]) -> bool:
+    return len(command) > 1 and command[0] == "draw"
+
+def is_help(command: list[str]) -> bool:
+    return len(command) == 1 and command[0] == "help"
 
 if __name__ == "__main__":
     path = "Storage.xlsx"
@@ -27,24 +30,35 @@ if __name__ == "__main__":
     drawer = Drawer()
 
     df = None
+    
+    print("Программа запущена. Введите help для получения списка команд программы.")
 
     while(True):
-        comand = utils.comand_to_list(input())
-        if is_exit(comand):
+        command = utils.command_to_list(input())
+        if is_exit(command):
             break
-        elif is_load(comand):
-            df = storage.load(sheet=comand[1], index_col="Date")
+        elif is_load(command):
+            df = storage.load(sheet=command[1], index_col="Date")
             print(df)
-        elif is_save(comand):
-            storage.save(df, sheet=comand[1])
-        elif is_download(comand):
-            series = storage.download(org=comand[1],
-                                  period=utils.str_to_timedelta(comand[2]),
-                                  interval=comand[3])
+        elif is_save(command):
+            storage.save(df, sheet=command[1])
+        elif is_download(command):
+            series = storage.download(org=command[1],
+                                  period=utils.str_to_timedelta(command[2]),
+                                  interval=command[3])
             df = utils.calculate_dataframe(series=series, 
-                                           interval=comand[3], 
-                                           window=comand[4])
-        elif is_draw(comand):
-            drawer.plot(df)
+                                           interval=command[3], 
+                                           window=command[4])
+        elif is_draw(command):
+            drawer.plot(df, command[1:])
+        elif is_help(command):
+            print("Список команд:")
+            print("exit - завершить программу.")
+            print("load [sheet name] - загрузить страницу [sheet name] из .xslx файла.")
+            print("save [sheet name] - сохранить данные в страницу [sheet name] .xslx файла.")
+            print("download [organization] [period] [interval] [window] - загружает данные указанно компании за указанный интервал с определенным периодом." + 
+                "Вычисляет все данные с указанным окном скользящего среднего.")
+            print("draw - Отобразить графики рассчитанных значений.")
+
         else:
             print("Не известная команда.")
--- plots/plots.py
+++ plots/plots.py
@@ -1,29 +1,76 @@
 import matplotlib.pyplot as plt
 from pandas import DataFrame
+import math
+
+from matplotlib.widgets import Button
 
 class Drawer:
+    '''
+    Класс для отрисовки графиков
+    '''
+
     def plot(self,
-             data: DataFrame):
-        self._plot_main(data)
-        self._plot_diff(data)
-        self._plot_autocor(data)
+             data: DataFrame, columns: list) -> None:
+        '''
+        Отрисовывает несколько графиков, по одному на каждый временной ряд
+        Входные параметры:
+            data - Таблица с данными для отрисовки
+            columns - Названия столбцов, графики которых надо отрисовать
+        '''
+
+        # Проверка на корректность введенных столбцов
+        for column in columns:
+            if not (column in list(data.columns.values)):
+                print("Неверное название столбца: ", column)
+                return
+
+        self._data = data
+        # Хранение всех рисунков на изображении для возможности редактирования
+        self._axs = []
+        self._fig = plt.figure()
+        self._cols = columns
+        
+        self._nrows = math.ceil(len(columns) / 2)
+        self._ncols = 2
+        if len(columns) == 1:
+            self._ncols = 1
+
+        # Определение кнопки 
+        resetax = self._fig.add_axes([0.8, 0.025, 0.1, 0.04])
+        button = Button(resetax, 'Reset', hovercolor='0.975')
+        button.on_clicked(self._reset);
+
+        # Отрисовка всех графиков
+        for i in range(0, len(columns)):
+            if (i == (len(columns) - 1)) and len(columns) % 2 == 1:
+                self._plot(columns[i], (i + 1, i + 2))
+            else:
+                 self._plot(columns[i], i + 1)
+
         plt.show()
+    
+    def _reset(self, event) -> None:
+        '''
+        Функция сборса масштаба всех графиков по обоим осям.
+        Используется, так как неудобно сбрасывать масштаб встроенными средствами matplotlib
+        '''
+        
+        for i in range(0, len(self._axs)):
+            self._axs[i].set_xlim(self._data.index[0], self._data.index[-1])
+            self._axs[i].set_ylim(self._data[self._cols[i]].min(), self._data[self._cols[i]].max())
+        plt.show()         
 
 
-    def _plot_main(self,
-                   data: DataFrame):
-        plt.subplot(2, 1, 1)
-        plt.plot(data.index, data["Open"])
-        plt.plot(data.index, data["Movavg"])
-
-
-    def _plot_diff(self,
-                   data: DataFrame):
-        plt.subplot(2, 2, 3)
-        plt.plot(data.index, data["Diff"])
-
-
-    def _plot_autocor(self,
-                      data: DataFrame):
-        plt.subplot(2, 2, 4)
-        plt.plot(data.index, data["Autocor"])
+    def _plot(self,
+                column: str, coord: int) -> None:
+        '''
+            Функция отрисовки одного графика на изображении
+            Входные параметры: 
+                column - название столбца, по которому необходимо построить график
+                coord - координаты графика на изображении
+        '''
+        
+        ax = self._fig.add_subplot(self._nrows, self._ncols, coord)
+        ax.plot(self._data.index, self._data[column])
+        ax.set_title(column)
+        self._axs.append(ax) 
